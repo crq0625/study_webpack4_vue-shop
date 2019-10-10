2c05f5623e91fee4bd21240b8499328887d28f7d
gather <chenruiqiang@yd-x.com>
2019-10-11 07:51:42 +0800

    初始化vue项目模板

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+<div>
+    <h1>webpack 编译 vue</h1>
+    <button type="button" class="mui-btn mui-btn-royal">紫色</button>
+</div>
+</template>
+<script>
+
+</script>
+<style lang="less" scoped>
+    
+</style>
--- src/main.js
+++ src/main.js
@@ -4,10 +4,14 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import '../src/lib/mui/css/mui.min.css'
 import './css/app.css'
 
-//导入vue内容。
+//导入vue入口文件
 import Vue from 'vue'
+
+//导入mint-ui style样式。
+import 'mint-ui/lib/style.css'
+
 //导入app组件
-import app from './app.vue'
+import app from './App.vue'
 
 //自定义路由
 import router from "./router.js";
@@ -19,10 +23,6 @@ import { Button } from 'mint-ui';
 //使用vue.component 注册组件
 Vue.component(Button.name, Button);
 
-//导入mint-ui style样式。
-import 'mint-ui/lib/style.css'
-
-
 
 //把mintUI挂载到Vue中。
 //Vue.use(MintUI) //把所有的组件都注册成全局的。
@@ -31,18 +31,6 @@ import 'mint-ui/lib/style.css'
 const vm = new Vue({
     el:'#app',
     data:{},
-    render:c=>c(app),
+    render:C=>C(app),
     router //路由对象挂载到vm上
 });
-/**
-    <router-link to="/account">account</router-link>
-    <router-link to="/goodList">goodlist</router-link>
-    <router-view></router-view>
-    需要写到app.vue中，因为index.html中的#app内容会被app.vue中的内容替换。
-    因此需要把上面的内容放到app.vue里面。
-
-     APP 组件是通过 vm 实例的render函数渲染出来的，render函数渲染出来的组件只能放到el:'#app'
-     所指定的元素中。
-     account.vue和GoodList.vue组件，是通过路由匹配监听到的，所以这两个组件只能展示到属于
-     路由的<router-view></router-view>中去。
- **/
